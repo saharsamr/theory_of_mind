125e58527ed2ba893797297e49ba9e2d98636389
saharsamr <sahar.rajabi76@gmail.com>
2020-10-18 03:13:46 +0330

    try to cleanup!

--- analysis/num_of_trials/glmfit.py
+++ analysis/num_of_trials/glmfit.py
@@ -2,36 +2,6 @@ import statsmodels.api as sm
 from patsy import dmatrices
 
 
-def clean_MFdata_for_glm(common_reward_MF, unique_reward_MF, repeat_MF):
-
-    data = np.empty((len(common_reward_MF), 3))
-    for i, (common_reward, unique_reward, repeat) in enumerate(zip(common_reward_MF, unique_reward_MF, repeat_MF)):
-
-        data[i][0] = common_reward
-        data[i][1] = unique_reward
-        data[i][2] = repeat
-
-    df = pd.DataFrame(data, columns=['common_reward', 'unique_reward', 'repeat'])
-    df = df.astype(dtype={'common_reward': 'int64', 'unique_reward': 'int64', 'repeat': 'int64'})
-
-    return df
-
-
-def clean_MBdata_for_glm(common_reward_MB, reward_prob_MB, repeat_MB):
-
-    data = np.empty((len(common_reward_MB), 3))
-    for i, (common_reward, reward_prob, repeat) in enumerate(zip(common_reward_MB, reward_prob_MB, repeat_MB)):
-
-        data[i][0] = common_reward
-        data[i][1] = reward_prob
-        data[i][2] = repeat
-
-    df = pd.DataFrame(data, columns=['common_reward', 'reward_prob', 'repeat'])
-    df = df.astype(dtype={'common_reward': 'int64', 'reward_prob': 'float64', 'repeat': 'int64'})
-
-    return df
-
-
 def fit_binomial_glm(subject_data, formula):
 
     y, x = dmatrices(formula, subject_data, return_type='dataframe')
