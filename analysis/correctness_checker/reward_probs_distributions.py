19dc3cc1fff436f1459e8cdb7ad8a37fdcbd7896
saharsamr <sahar.rajabi76@gmail.com>
2020-12-07 22:16:45 +0330

    refactor and plot all 3 faces reward probabilities

--- analysis/correctness_checker/reward_probs_distributions.py
+++ analysis/correctness_checker/reward_probs_distributions.py
@@ -6,17 +6,25 @@ import numpy as np
 import json
 
 
-def plot_rewards_probs_distr(data_path, subject_id):
+def plot_all_phases_reward_probs(data_path, subject_id):
 
-    probs = extract_reward_probs(data_path, subject_id, 'task-phase1')
+    plot_phase_reward_probs(data_path, subject_id, 'task-phase1')
+    plot_phase_reward_probs(data_path, subject_id, 'task-phase2')
+    plot_phase_reward_probs(data_path, subject_id, 'task-phase3')
 
+    plt.show()
+
+
+def plot_phase_reward_probs(data_path, subject_id, phase_name):
+
+    probs = extract_reward_probs(data_path, subject_id, phase_name)
+    fig = plt.figure(phase_name)
     xs = [x for x in range(len(probs[0]))]
     plt.plot(xs, probs[0], label='p0')
     plt.plot(xs, probs[1], label='p1')
     plt.plot(xs, probs[2], label='p2')
     plt.plot(xs, probs[3], label='p3')
     plt.legend()
-    plt.show()
 
 
 def extract_reward_probs(data_path, subject_id, phase_name):
