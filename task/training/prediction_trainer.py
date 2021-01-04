32904f33aa95859b1770e7fa542cbd9988a9219e
saharsamr <sahar.rajabi76@gmail.com>
2021-01-04 16:38:03 +0330

    check if subject can pass

--- task/training/prediction_trainer.py
+++ task/training/prediction_trainer.py
@@ -11,7 +11,11 @@ class PredictionTrainer:
     available_objects = []
     rewards = []
     agent_selections = []
+    user_predictions = []
     objects_actual_rewards = []
+
+    agent_selection_responses = []
+    user_prediction_responses = []
 
     @classmethod
     def start_training(cls, presenter):
@@ -19,15 +23,37 @@ class PredictionTrainer:
         passed, round_ = False, 0
         while not passed:
             cls.set_round_info()
-            agent_selections, user_selections, user_predictions = \
+            agent_selections, user_selections = \
                 cls.run_training_round(
                     presenter, 4*cls.num_training_repeat,
                     cls.trial_pairs[round_], cls.available_objects[round_],
                     cls.agent_selections[round_], cls.rewards[round_]
             )
+            cls.user_prediction_responses.append(user_selections)
+            cls.agent_selection_responses.append(agent_selections)
 
-            if agent_selections == cls.agent_selections[round_] and user_predictions == user_selections:
+            if cls.is_passed(agent_selections, user_selections, round_):
                 passed = True
+            round_ += 1
+
+    @classmethod
+    def is_passed(cls, agent_selection_responses, user_selection_responses, round_):
+
+        if agent_selection_responses == cls.agent_selections[round_] \
+                and user_selection_responses == cls.user_predictions[round_]:
+
+            same_selection, different_selections = False, False
+            for agent, user in zip(cls.agent_selections[round_], cls.user_predictions[round_]):
+                if same_selection and different_selections:
+                    break
+                if agent == user:
+                    same_selection = True
+                else:
+                    different_selections = True
+
+            return same_selection == different_selections
+
+        return False
 
     @classmethod
     def run_training_round(
@@ -35,7 +61,7 @@ class PredictionTrainer:
             real_selections, rewards, time_limit=float('inf')
     ):
 
-        agent_selections, user_selections, reaction_times, user_predictions = [], [], [], []
+        agent_selection_response, user_selection_response, user_predictions = [], [], []
         for t in range(n_trials):
 
             real_selected_index = 0 if trial_pairs[t][0] == real_selections[t] else 1
@@ -50,10 +76,11 @@ class PredictionTrainer:
                 presenter.present_prediction_training_step(
                     trial_pairs[t], real_selections[t], prediction, time_limit
             )
-            agent_selections.append(agent_selected)
-            user_selections.append(user_selected)
+            agent_selection_response.append(agent_selected)
+            user_selection_response.append(user_selected)
 
-        return agent_selections, user_selections, user_predictions
+        cls.user_predictions.append(user_predictions)
+        return agent_selection_response, user_selection_response
 
     @classmethod
     def set_round_info(cls):
